9bba76369221dbf7d61b4bfa9044c95670830d10
gurvan-dev <jurwane@protonmail.ch>
2022-02-01 13:56:38 +0100

    ptite matrice

--- modules/matrice.py
+++ modules/matrice.py
@@ -8,14 +8,21 @@ def random_int_list(n, bound):
 
 def random_matrix(n, bound, null_diag = False, symmetric = False, oriented = False, triangular = False):
     mat = []
-    max_bound = bound
-    if symmetric:
-        max_bound = bound / 2
     for i in range(n):
-        mat.append(random_int_list(n, max_bound))
+        mat.append(random_int_list(n, bound))
+        # [i][j] > 0 => [j][i] = 0
         if symmetric:
             for j in range(max_bound):
                 map[i].append(map[i][max_bound-j])
-        if null_diag:
-            mat[i][i] = 0
+            
+    for x in range(n):
+         for y in range(n):
+            if null_diag and x == y:
+                mat [x][y] = 0
+            if oriented and mat[x][y] > 0:
+                mat[y][x] = 0
+            if symmetric:
+                mat[y][x] = mat[x][y]
+            if triangular and x > j:
+                mat[x][y] = 0
     return mat
