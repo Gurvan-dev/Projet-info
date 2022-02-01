5db9d772a80ee42814645e3c8049b1fb09574542
gurvan-dev <jurwane@protonmail.ch>
2022-02-01 13:48:22 +0100

    on est la

--- TODO.md
+++ TODO.md
@@ -1,17 +1,7 @@
 # PROJET-INFO
 
-## Divers
+## Matrices
 
-- [x] Relire ce qui est déjà fait.
-  
-## Open Digrah
-
-- [x] Finir is_well_formed
-- [x] Ajouter documentations sur les diverses fonctions
-
-## Open Digraph Test
-
-- [x] Tester is_well_formed
-- [x] Tester "Add Input Node"
-- [x] Tester "Add Output Node"
-- [x] Commenter les tests
+- [.] Générer matrices random sans paramètre
+- [.] Générer matrices random avec diagonales vide
+- [.] Générer matrices random avec symmétrie
--- modules/matrice.py
+++ modules/matrice.py
@@ -8,8 +8,14 @@ def random_int_list(n, bound):
 
 def random_matrix(n, bound, null_diag = False, symmetric = False, oriented = False, triangular = False):
     mat = []
+    max_bound = bound
+    if symmetric:
+        max_bound = bound / 2
     for i in range(n):
-        mat.append(random_int_list(n, bound))
+        mat.append(random_int_list(n, max_bound))
+        if symmetric:
+            for j in range(max_bound):
+                map[i].append(map[i][max_bound-j])
         if null_diag:
             mat[i][i] = 0
     return mat
